8aae4f45e9ad45d03c5db8215c71737bfaae2d46
Cardoso Nicolás <cardoso.leandro.nicolas@gmail.com>
2020-07-19 21:30:57 -0300

    add pg configuration

--- src/database/db.js
+++ src/database/db.js
@@ -1,14 +1,12 @@
 const { Sequelize } = require("sequelize");
 const database = require("../config/dbConfig");
+const dbURL = process.env.DATABASE_URL;
 
-const sequelize = new Sequelize(
-  database.database,
-  database.usermane,
-  database.password,
-  {
-    dialect: "sqlite",
-    storage: "./src/database/db.sqlite",
-  }
-);
+const sequelize = dbURL
+  ? new Sequelize(dbURL)
+  : new Sequelize(database.database, database.usermane, database.password, {
+      dialect: "sqlite",
+      storage: "./src/database/db.sqlite",
+    });
 
 module.exports = sequelize;
